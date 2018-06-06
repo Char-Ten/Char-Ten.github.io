b5b223fb76aa25e97644dbc01747a42d5b17ce5a
CharTen <645070346@qq.com>
2018-06-06 18:49:04 +0800

    Update index.js

--- demo/maptalk/index.js
+++ demo/maptalk/index.js
@@ -2,7 +2,7 @@ var map = new maptalks.Map('map', {
     center: new maptalks.Coordinate([113.26445102691652, 23.129704666325214]),
     zoom: 18,
     baseLayer: new maptalks.TileLayer('base', {
-        urlTemplate: 'https://mt2.google.cn/vt/lyrs=s@110&hl=zh-CN&gl=CN&src=app&x={x}&y={y}&z={z}&s={s}',
+        urlTemplate: 'https://mt2.google.cn/vt/lyrs=m@110&hl=zh-CN&gl=CN&src=app&x={x}&y={y}&z={z}&s={s}',
         subdomains: ['a', 'b', 'c']
     })
 });
